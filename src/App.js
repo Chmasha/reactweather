85207dfe436b18f9b832e8b331dc245f7f947d14
Chmasha <chorna.mariia@gmail.com>
2022-10-03 21:01:32 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Mariia Chorna and is{" "}
+        <a
+          href="https://github.com/Chmasha/reactweather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sources on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
